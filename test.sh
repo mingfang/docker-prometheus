19ac564fbc852b172f4475b9244faa4b10424dc5
root <root@minux.home>
2015-12-16 00:33:57 -0500

    retain failed test config file for debugging

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-confd --onetime --log-level debug --confdir /etc/confd --backend etcd --node $ETCD_HOST:4001 --watch
+confd --onetime --log-level debug --confdir /etc/confd --backend etcd --node $ETCD_HOST:4001 --watch -keep-stage-file 
